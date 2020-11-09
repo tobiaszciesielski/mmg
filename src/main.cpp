27d87ebca2b901e8867a1fb9317579a01f65f996
tobiaszciesielski <t.ciesielski1177@gmail.com>
2020-11-09 01:43:13 +0100

    Resending byte as string

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,15 @@
-#include <Arduino.h>
-  
-// Additional libraries
-#include "PubSubClient.h"
-#include "WiFi.h"
-#include "ArduinoJson.h"
+#include "../include/config.hpp"
 
-// Configuration variables  
-#define WIFI_NETWORK "Ciesielski Hot Spot"
-#define WIFI_PASSWORD "Aneta1999"
-#define WIFI_TIMEOUT_MS 15000
-#define MQTT_SERVER_IP "192.168.8.120"
-#define MQTT_SERVER_PORT 1883
-#define SENDING_DATA_FREQ 1000
-unsigned long messageSendingTime = (1.0 / SENDING_DATA_FREQ) * 1000;
-  
 // MQTT Client
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
-  
+
 void connectToWifi() {
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_NETWORK);
-  
+  if (DEBUG) { 
+    Serial.print("Connecting to ");
+    Serial.print(WIFI_NETWORK);
+  }
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
   
@@ -29,65 +17,91 @@ void connectToWifi() {
   
   while (WiFi.status() != WL_CONNECTED &&
           millis() - startAttemptTime < WIFI_TIMEOUT_MS) {
-    Serial.print(".");
-    delay(1000);
+    if (DEBUG) {
+      Serial.print(".");
+      delay(1000);
+    }
   }
   
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connection Failed !");
-  } else {
-    Serial.print("Connected to");
-    Serial.println(WiFi.localIP());
+  if (DEBUG) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.print("Connection Failed !");
+    } else {
+      Serial.print("Connected to");
+      Serial.println(WiFi.localIP());
+    }
   }
 }
   
-void reconnect() {
+
+void mqttReconnect() {
   while (!mqttClient.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    if (DEBUG) Serial.print("Attempting MQTT connection...");
+
     WiFi.mode(WIFI_STA);
-    if (mqttClient.connect("esp32client")) {
-      Serial.println("connected");  
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
+    bool isConnected = mqttClient.connect("esp32client");
+    
+    if (DEBUG) {
+      if (isConnected) {
+        Serial.println("connected!\n"); 
+      } else {
+        Serial.print("failed, rc=");
+        Serial.print(mqttClient.state());
+        Serial.println(" try again in 5 seconds");
+        delay(5000);
+      }
     }
   }
 }
 
-void sendData(char* message, size_t messageLenght) {
-  mqttClient.publish("test", message, messageLenght);
+
+void sendData(char* message, size_t messageLenghta) {
+  mqttClient.publish("test", message, messageLenghta);
 }
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
   connectToWifi();
-  mqttClient.setBufferSize(1024);
   mqttClient.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
-  Serial.print("The message sending interval is set to (ms): ");
-  Serial.print(messageSendingTime);
 }
 
-unsigned long lastMessageTimeStamp = 0;
-StaticJsonDocument<1024> jsonBuffer;
-JsonArray data = jsonBuffer.createNestedArray("data");
-char buffer[256];
+
 void loop() {
-  if(!mqttClient.connected())
-    reconnect();
-
-  mqttClient.loop();
-  data.add(int(random(0,10)));
   
-  if(millis() - lastMessageTimeStamp > messageSendingTime) {
-    int n = serializeJson(jsonBuffer, buffer);
-    sendData(buffer, n);
+  // Start transmission transmission of HEX-type data (0x21 is !)
+  Serial.write(0x21);
 
-    jsonBuffer.clear();
-    data = jsonBuffer.createNestedArray("data");
-    jsonBuffer["interval"] = millis() - lastMessageTimeStamp;
-    lastMessageTimeStamp = millis();
+  while (mqttClient.connected()) {
+    mqttClient.loop();
+
+    if (Serial.available()) {
+      // read byte
+      unsigned char byte = Serial.read();
+
+      // convert byte to string
+      char sample[3];
+      sprintf(sample, "%x", byte);     
+
+      // send sample
+      sendData(sample, 3);                                    
+    }
   }
+
+  mqttReconnect();
 }
 
+
+
+
+// NOTES 
+
+// 72 ints
+// 8 mcu * 3 canals * 3 values * 2 bytes = 144 bytes
+// short data[72] {0};
+
+
+// text sample : #@8;676;415;-4092;59;-41;14;631;207;681;#
+
+
+// 40 02 48 05 57 02 DF 0E 0C 00 0A 00 18 00 24 FF 66 00 B7 F2 23
