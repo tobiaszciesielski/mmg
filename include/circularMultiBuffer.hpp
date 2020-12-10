01ef4c537dd90a9b8a3facbf694cdc644776ac9c
tobiaszciesielski <t.ciesielski1177@gmail.com>
2020-12-10 16:07:38 +0100

    Boolean danceing with reading frame

--- include/circularMultiBuffer.hpp
+++ include/circularMultiBuffer.hpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <vector>
+#include <chrono>
 
 template <typename T>
-class CircularMultiBuffer {
+class CircularBuffer {
 private:
   std::vector<std::vector<T>> buffer;
   size_t head;
@@ -10,6 +12,7 @@ private:
   size_t cols;
   size_t expectedRow;
   bool isFull;
+  T type_maximum = std::numeric_limits<T>::max();
 
 private:
   void pushHead() {
@@ -32,7 +35,7 @@ private:
       to = row;
       
       for(int i = from; i < to; i++) {
-        buffer[i][head] = T();
+        buffer[i][head] = type_maximum;
       }
     } else if (row < expectedRow) {
       // fill the rest with 0 and move head
@@ -45,14 +48,14 @@ private:
       for(int i = 0; i < steps; i++) { 
         pos = (from + i) % rows;
         if(pos == 0) pushHead();
-        buffer[pos][head] = T();
+        buffer[pos][head] = type_maximum;
       }
     }
     expectedRow = row;
 }
 
 public:
-  CircularMultiBuffer(size_t bufferRows, size_t bufferCols): 
+  CircularBuffer(size_t bufferRows, size_t bufferCols): 
     buffer(std::vector<std::vector<T>>(bufferRows)),
     head(0), 
     tail(0), 
@@ -63,6 +66,7 @@ public:
       for (size_t i = 0; i < bufferRows; i++) buffer[i] = std::vector<T>(bufferCols);      
     }
 
+  // insert value into passed position
   void insert(T value, size_t row) {   
     if(row != expectedRow) 
       fillWithZeros(row);
@@ -76,6 +80,7 @@ public:
     }
   }
 
+  // get package (column)
   std::vector<T> get() {
     if (isEmpty()) return std::vector<T>(rows, T());
     
@@ -89,25 +94,30 @@ public:
     return tmpVector;
   }
 
+
+  // delete all values from buffer
   void clear() {
     expectedRow = 0;
     head = tail;
     isFull = false;
   }
 
+  // check if buffer is empty
   bool isEmpty() {
     return (!isFull && (head == tail));
   }
 
+  // check if buffer is filled
   bool isFilled() {
     return isFull;
   }
 
-  // get total size of buffer
+  // get package length
   size_t getColumsCount() {
     return cols;
   }
 
+  // get buffer length
   size_t getRowsCount() {
     return rows;
   }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../include/config.hpp"
+#include <base64.h>
 
 // MQTT Client
 WiFiClient espClient;
@@ -6,11 +7,13 @@ PubSubClient mqttClient(espClient);
 
 void sendData(const uint8_t* message, size_t messageLenght) {
     if (messageLenght > mqttClient.getBufferSize()) {
-      // strcpy((char*)message,"Pocket too big!"), messageLenght=16;
       mqttClient.publish("test", "Pocket too big!",false);
-    } else
-    mqttClient.publish("test", message,messageLenght,false);
+    } else {
+      mqttClient.publish("test", message,messageLenght,false);
+    }
   }
+
+
 
 void connectToWifi() {
   if (DEBUG) { 
@@ -74,17 +77,18 @@ void setup() {
 void loop() {
   
   // neccesary variables declaration
-  // char bigBuffer[SERIAL_BUFFER_SIZE];
-
   uint bufferPosition = 0;
   const uint16_t buffSize = 1000; 
   uint8_t buffer[buffSize];
 
+  int8_t counter = 0;
+
   unsigned long lastTimeSend = 0;
   unsigned long diff;
-  
+  char message[15];
   int margin = 0;
   uint aviableBytes = 0;
+  CircularBuffer<unsigned short> circularBuffer(72, 8);
 
   // Start transmission transmission of HEX-type data (0x21 is !)
   Serial.write(0x21);
@@ -103,9 +107,56 @@ void loop() {
           Serial.read();
         }
       } else {
+        int8_t sample;
+        bool is_frame_started = false;
+        bool is_inside_frame = false;
+        bool first_byte = false;
+        bool second_byte = false;
+        uint8_t expected_row = 0;
+        int8_t first = 0;
+
+
         // Store data in buffer
         for (; bufferPosition < margin; bufferPosition++) {
-          buffer[bufferPosition] = Serial.read();
+          sample = Serial.read();
+          
+          // start of frame
+          if (sample == 0x40 && !is_frame_started) { 
+            is_frame_started = true;
+          
+          // frame number
+          } else if (is_frame_started && (sample >= 1) && (sample <= 8) && !is_inside_frame) { 
+            expected_row = sample;
+            is_inside_frame = true;
+            first_byte = true;
+          
+          // end of frame
+          } else if (sample != 0x23) { 
+            is_inside_frame = false;
+            is_frame_started = false;
+            continue;
+
+          // read first byte
+          } else if (first_byte) {
+            first = sample;
+            first_byte = false;
+            second_byte = true;
+          
+          // read second byte and insert into buffer
+          } else if (second_byte) {
+            short value = short(
+              (unsigned char)(first) << 8 |
+              (unsigned char)(sample)
+            );
+
+            circularBuffer.insert(value, counter);
+            counter += expected_row;
+
+            if(counter >= 72) counter = 0;
+            
+            first_byte = true;
+            second_byte = false;
+          }
         }    
       }
     }
@@ -114,11 +165,12 @@ void loop() {
     diff = micros() - lastTimeSend;
     if (diff >= dataTimeSendMicrosec) {
       if (bufferPosition > 0) {
-
-        sendData(buffer, bufferPosition);
-
+        base64::encode()
+        int n = sprintf(message, "%u", circularBuffer.getCapacity());
+        mqttClient.publish("test", message);
         lastTimeSend = micros();
         bufferPosition = 0;
+        circularBuffer.clear();
       }
     }
   } 
